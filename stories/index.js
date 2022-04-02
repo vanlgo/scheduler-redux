25e75441ffb6c004e1453fa9abde2617aecb13be
vanlgo <goellevan@gmail.com>
2022-04-02 17:06:49 -0400

    created confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -150,9 +151,14 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-  .add('Show', () => <Show
+  .add("Show", () => <Show
     student={"Lydia Miller-Jones"}
     interviewer={interviewers[1]}
     onEdit={action("onEdit")}
     onDelete={action("onDelete")}
-  />);
+  />)
+  .add("Show", () => <Confirm
+    message={"Delete the appointment?"}
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
