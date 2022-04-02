36dcff98159896cd36c212326e8ba16f0dccfe44
vanlgo <goellevan@gmail.com>
2022-04-02 19:43:40 -0400

    added schedule to object

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
+import Appointment from "./Appointment";
+
 const days = [
   {
     id: 1,
@@ -21,6 +23,44 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
@@ -50,7 +90,12 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => {
+          return (<Appointment
+            key={appointment.id}
+            {...appointment}
+          />)
+        })}
       </section>
     </main>
   );
