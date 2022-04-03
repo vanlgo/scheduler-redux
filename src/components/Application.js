8831d87d126d4fa39c0dd8e50c2a2c74af8aa536
vanlgo <goellevan@gmail.com>
2022-04-02 23:50:21 -0400

    refactored application to use get interview and moved appointments to schedule map

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,10 +3,10 @@ import axios from "axios";
 
 import "components/Application.scss";
 
-import { getAppointmentsForDay } from "helpers/selectors";
-
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -15,9 +15,22 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  
 
-  const setDay = day => setState({ ...state, day });
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   useEffect(() => {
     // data sets being requested from api
@@ -26,14 +39,15 @@ export default function Application(props) {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
     ]).then((all) => {
-      setState({
+      setState((prev) => ({
+        ...prev,
         days: all[0].data,
         appointments: all[1].data,
-        interviewers: all[2].data
-      });
+        interviewers: all[2].data,
+      }));
     });
   }, [])
-
+  const setDay = day => setState({ ...state, day });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -57,12 +71,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => {
-          return (<Appointment
-            key={appointment.id}
-            {...appointment}
-          />)
-        })}
+        {schedule}
         {<Appointment key="last" time="5pm" />}
       </section>
     </main>
